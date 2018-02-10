524122cbf5e1b859456efc5de11603bf82c22b38
Jhon Gil Sepulveda <jhongil96@gmail.com>
2018-02-09 22:24:17 -0500

    Testing

--- routes/main.js
+++ routes/main.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const chatRoutes = require('./api/chat.js');
-const userRoutes = require('./api/user.js');
+//const chatRoutes = require('./api/chat.js');
+//const userRoutes = require('./api/user.js');
+const ChatController = require('../../Controllers/ChatController.js');
 
 module.exports = (app) => {
     const apiRoutes = express.Router();
 
-    apiRoutes.use('/chat', chatRoutes);
-    apiRoutes.use('/user', userRoutes);
+    //apiRoutes.use('/chat', chatRoutes);
+    //apiRoutes.use('/user', userRoutes);
 
-    app.use('/', apiRoutes);
+    app.use('/', ChatController.getConversations);
 }
